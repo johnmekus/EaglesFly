c668703606e96335f9d26987cc133de894c9fdd4
johnmekus <mekus@bc.edu>
2021-12-05 23:59:20 -0500

    App Complete!

---
+++ EaglesFly/EventTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  EventTableViewCell.swift
+//  EaglesFly
+//
+//  Created by John Mekus on 12/1/21.
+//
+
+import UIKit
+
+class EventTableViewCell: UITableViewCell
+{
+    @IBOutlet weak var locationNameLabel: UILabel!
+    @IBOutlet weak var dateAndTimeLabel: UILabel!
+}
